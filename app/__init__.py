8eebb167a56cd20f2408f69b210f1a0b104e81ec
oscar <oscar.moreno@cti.espol.edu.ec>
2025-04-09 04:39:35 -0500

    cambios

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import task_routes
 from flasgger import Swagger
 
+import os
+
 def create_app():
     app = Flask(__name__)
+
+    origins = os.getenv("FRONTEND_ORIGINS", "http://localhost:5174").split(",")
+
+    CORS(app, origins=origins, supports_credentials=True, allow_headers=[
+        "Content-Type", 
+        "Authorization", 
+        "X-Amz-Date",
+        "X-Api-Key",
+        "X-Amz-Security-Token"
+    ], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"])
+
     Swagger(app)
     app.register_blueprint(task_routes)
     return app
