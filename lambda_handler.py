b9fbea2ac82c264285c5a771d334f17af92a1bc8
oscar <oscar.moreno@cti.espol.edu.ec>
2025-04-09 01:32:06 -0500

    validaciones de evento en el lambda handler

--- lambda_handler.py
+++ lambda_handler.py
@@ -4,4 +4,21 @@ from aws_lambda_wsgi import response
 flask_app = create_app()
 
 def lambda_handler(event, context):
-    return response(flask_app, event, context)
+    if 'queryStringParameters' not in event:
+        event['queryStringParameters'] = None
+
+    if 'requestContext' not in event:
+        event['requestContext'] = {"protocol": "HTTP/1.1"}
+
+    if 'headers' not in event:
+        event['headers'] = {}
+    if 'Host' not in event['headers']:
+        event['headers']['Host'] = "localhost"
+
+    
+    return response(
+        flask_app, 
+        event, 
+        context,
+        environ_builder_kwargs={"wsgi.url_scheme": "https"}
+    )
